174b8d5a5c2137218a965712f24157f353ffeb7c
Janis <janis.zesiger@gmail.com>
2022-05-22 12:51:29 +0200

    Added requirements

--- test.py
+++
@@ -1,5 +0,0 @@
-# Import summarizer
-from text_summarizer import summarizer
-
-
-summarizer.summarize("Just a random text i'm writing nothing really important but it has to be long so it can be summarized.", "textrank", 0.5)
